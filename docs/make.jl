822863ddeee927a34b09e50d685cc05332afdd78
Aldo <ealdoglielmo@gmail.com>
2024-05-22 09:34:53 +0200

    docs: remove target from deploydocs in make

--- docs/make.jl
+++ docs/make.jl
@@ -47,4 +47,4 @@ makedocs(
     ],
 )
 
-deploydocs(;repo = "github.com/bancaditalia/BeforeIT.jl.git", devbranch = "main", target = "gh-pages")
+deploydocs(;repo = "github.com/bancaditalia/BeforeIT.jl.git", devbranch = "main")
