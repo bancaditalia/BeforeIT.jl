064d4caca13fc69e745def4c5687da4fe1e37a2e
Aldo <ealdoglielmo@gmail.com>
2024-09-26 09:26:35 +0200

    improve plot_data and plot_data_vector, update basic_example

--- examples/basic_example.jl
+++ examples/basic_example.jl
@@ -3,7 +3,7 @@
 # We start by importing the BeforeIT library and other useful libraries.
 
 import BeforeIT as Bit
-using FileIO, Plots, StatsPlots
+using FileIO, Plots
 
 
 # We then initialise the model loading some precomputed set of parameters and by specifying a number of epochs.
@@ -43,18 +43,12 @@ end
 
 # We can then plot any time series stored in the data tracker, for example
 
-p1 = plot(data.real_gdp, title = "gdp", titlefont = 10)
-p2 = plot(data.real_household_consumption, title = "household cons.", titlefont = 10)
-p3 = plot(data.real_government_consumption, title = "gov. cons.", titlefont = 10)
-p4 = plot(data.real_capitalformation, title = "capital form.", titlefont = 10)
-p5 = plot(data.real_exports, title = "exports", titlefont = 10)
-p6 = plot(data.real_imports, title = "imports", titlefont = 10)
-p7 = plot(data.wages, title = "wages", titlefont = 10)
-p8 = plot(data.euribor, title = "euribor", titlefont = 10)
-p9 = plot(data.nominal_gdp ./ data.real_gdp, title = "gdp deflator", titlefont = 10)
+plot(data.real_gdp, title = "gdp", titlefont = 10)
 
+# Or we can plot multiple time series at once using the function `plot_data`
 
-plot(p1, p2, p3, p4, p5, p6, p7, p8, p9, layout = (3, 3), legend = false)
+ps = Bit.plot_data(data, quantities = [:real_gdp, :real_household_consumption, :real_government_consumption, :real_capitalformation, :real_exports, :real_imports, :wages, :euribor, :gdp_deflator])
+plot(ps..., layout = (3, 3), legend = false)
 
 # To run multiple monte-carlo repetitions in parallel we can use
 
@@ -69,38 +63,8 @@ Threads.nthreads()
 # To activate Julia with a specific number of threads, say 8, you can use the command
 # `julia -t 8` in the terminal.
 
-# We can then plot the results of the monte-carlo repetitions.
-# Since we are saving the initial data point, we effectively have T+1 data points in our time series.
+# We can then plot the results of the monte-carlo repetitions using the function `plot_data_vector`
 
-Te = T + 1
+ps = Bit.plot_data_vector(data_vector)
+plot(ps..., legend = false)
 
-p1 = errorline(1:Te, data_vector.real_gdp, errorstyle = :ribbon, title = "gdp", titlefont = 10)
-p2 = errorline(
-    1:Te,
-    data_vector.real_household_consumption,
-    errorstyle = :ribbon,
-    title = "household cons.",
-    titlefont = 10,
-)
-p3 =
-    errorline(1:Te, data_vector.real_government_consumption, errorstyle = :ribbon, title = "gov. cons.", titlefont = 10)
-p4 = errorline(1:Te, data_vector.real_capitalformation, errorstyle = :ribbon, title = "capital form.", titlefont = 10)
-p5 = errorline(1:Te, data_vector.real_exports, errorstyle = :ribbon, title = "exports", titlefont = 10)
-p6 = errorline(1:Te, data_vector.real_imports, errorstyle = :ribbon, title = "imports", titlefont = 10)
-p7 = errorline(1:Te, data_vector.wages, errorstyle = :ribbon, title = "wages", titlefont = 10)
-p8 = errorline(1:Te, data_vector.euribor, errorstyle = :ribbon, title = "euribor", titlefont = 10)
-p9 = errorline(
-    1:Te,
-    data_vector.nominal_gdp ./ data.real_gdp,
-    errorstyle = :ribbon,
-    title = "gdp deflator",
-    titlefont = 10,
-)
-plot(p1, p2, p3, p4, p5, p6, p7, p8, p9, layout = (3, 3), legend = false)
-
-plot(p1, p4, p5, p3, p8, p9, layout = (3, 2), legend = false, size = (400, 600), dpi = 300, left_margin = 3Plots.mm) 
-
-plot(p1, p4, p5, p3, p8, p9, layout = (2, 3), legend = false, size = (600, 400), dpi = 300)#, left_margin = 3Plots.mm) 
-
-
-savefig("output.png")
