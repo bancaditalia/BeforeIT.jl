0614e9c2108db229405a587fa606b6d56cd15e67
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-10-27 21:34:32 +0100

    Update wsample.jl

--- src/utils/wsample.jl
+++ src/utils/wsample.jl
@@ -4,12 +4,12 @@ weighted sampling single implementation
 """
 function wsample_single(a, w)
     stop_w = rand() * sum(w)
-    partial_w = first(w)
+    partial_w = 0.0
     j = 1
-    for i in 2:length(w)
+    for i in 1:length(w)
         @inbounds partial_w += w[i]
         if partial_w > stop_w
-            j = i - 1
+            j = i
             break
         end
     end
