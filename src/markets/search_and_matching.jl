b563c6af2d818c0fde28d006e23dda00cd16fb51
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-11-11 01:26:28 +0100

    Update search_and_matching.jl

--- src/markets/search_and_matching.jl
+++ src/markets/search_and_matching.jl
@@ -581,7 +581,7 @@ end
 
 function create_weighted_sampler(P_f, S_f, F_g)
     w_cum_f_ = compute_price_size_weights(P_f, S_f, F_g)
-    sampler = WDynSampler()
+    sampler = DynamicSampler()
     sizehint!(sampler, length(F_g))
     append!(sampler, (1:length(F_g), w_cum_f_))
     return sampler
