bd0649309c4155c3b11f0f9b59e2ee0109f3aabb
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-11-28 23:59:31 +0100

    Update scenario_analysis_via_overload.jl

--- examples/scenario_analysis_via_overload.jl
+++ examples/scenario_analysis_via_overload.jl
@@ -1,21 +1,23 @@
 # # Scenario analysis via function overloading
 
-# In this tutorial we will illustrate how to perform a scenario analysis by running the model multiple times
-# under a specific shock and comparing the results with the unshocked model.
+# In this tutorial we will illustrate how to perform a scenario analysis by running
+# the model multiple times under a specific shock and comparing the results with the
+# unshocked model.
 
 import BeforeIT as Bit
 using Plots, StatsPlots
 
-
 parameters = Bit.AUSTRIA2010Q1.parameters
 initial_conditions = Bit.AUSTRIA2010Q1.initial_conditions
 
-# initialise the model and the data collector
+# Initialise the model and the data collector
+
 T = 20
 model = Bit.init_model(parameters, initial_conditions, T);
 data = Bit.init_data(model);
 
 # Simulate the model for T quarters
+
 data_vec_baseline = Bit.run_n_sims(model, 4)
 
 # Now, apply a shock to the model and simulate it again
@@ -27,7 +29,6 @@ function central_bank_rate(cb::Bit.CentralBank, model::Bit.Model)
     gamma_EA = model.rotw.gamma_EA
     pi_EA = model.rotw.pi_EA
     taylor_rate = Bit.taylor_rule(cb.rho, cb.r_bar, cb.r_star, cb.pi_star, cb.xi_pi, cb.xi_gamma, gamma_EA, pi_EA)
-
     if model.agg.t < 10
         return 0.01
     else
@@ -59,13 +60,7 @@ StatsPlots.errorline!(
     ylabel = "GDP",
 )
 
-# Note that, importantly, once the function central_bank_rate has been changed, the model will use the new 
-# interest rate in all the simulations, unless the function is changed again.
-# To restore the original interest rate, you need to close and restart the Julia session.
-
-function central_bank_rate(cb::Bit.CentralBank, model::Bit.Model)
-    gamma_EA = model.rotw.gamma_EA
-    pi_EA = model.rotw.pi_EA
-    taylor_rate = Bit.taylor_rule(cb.rho, cb.r_bar, cb.r_star, cb.pi_star, cb.xi_pi, cb.xi_gamma, gamma_EA, pi_EA)
-    return taylor_rate
-end
+# Note that, importantly, once the function `central_bank_rate` has been changed,
+# the model will use the new interest rate in all the simulations, unless the
+# function is changed again. To restore the original interest rate, you could
+# close and restart the Julia session.
