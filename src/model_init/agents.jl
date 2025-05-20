803f6b23d36b7230c80fc42843f28a71422a8e52
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-05-20 22:22:46 +0200

    fix tests

--- src/model_init/agents.jl
+++ src/model_init/agents.jl
@@ -226,18 +226,18 @@ mutable struct Model{W1<:AbstractWorkers,W2<:AbstractWorkers,
     rotw::R
     agg::A
     prop::P
-end
-
-function Model(w_act::W1, w_inact::W2, firms::F, bank::B, cb::C, gov::G, rotw::R, agg::A, prop::P) where {
-    W1<:AbstractWorkers, W2<:AbstractWorkers, F<:AbstractFirms, B<:AbstractBank,
-    C<:AbstractCentralBank, G<:AbstractGovernment, R<:AbstractRestOfTheWorld, A<:Aggregates, P
-}
-    model = Model{W1,W2,F,B,C,G,R,A,P}(w_act, w_inact, firms, bank, cb, gov, rotw, agg, prop)
+    function Model(w_act::AbstractWorkers, w_inact::AbstractWorkers, firms::AbstractFirms, 
+        bank::AbstractBank, cb::AbstractCentralBank, gov::AbstractGovernment, rotw::AbstractRestOfTheWorld, agg::A, prop::P) where {
+        W1<:AbstractWorkers, W2<:AbstractWorkers, F<:AbstractFirms, B<:AbstractBank,
+        C<:AbstractCentralBank, G<:AbstractGovernment, R<:AbstractRestOfTheWorld, A<:Aggregates, P
+    }
+        model = new{W1,W2,F,B,C,G,R,A,P}(w_act, w_inact, firms, bank, cb, gov, rotw, agg, prop)
     
-    # update model variables with global quantities (total income, total deposits) obtained from all the agents
-    update_variables_with_totals!(model)
+        # update model variables with global quantities (total income, total deposits) obtained from all the agents
+        update_variables_with_totals!(model)
 
-    return model
+        return model
+    end
 end
 
 # helper functions
