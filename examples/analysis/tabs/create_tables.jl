e454ae2ccd37345792d052f3945ab1502f6e4683
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-06-15 00:58:54 +0200

    Update create_tables.jl

--- examples/analysis/tabs/create_tables.jl
+++ examples/analysis/tabs/create_tables.jl
@@ -14,26 +14,18 @@ for calibration_date in collect(DateTime(2010, 03, 31):Dates.Month(3):DateTime(2
     save("data/italy/initial_conditions/$(year(calibration_date))Q$(Dates.quarterofyear(calibration_date)).jld2", init_conds)
 end
 
-for year in 2010:2019
-    for quarter in 1:4
-        parameters = load("data/italy/parameters/$(year)Q$(quarter).jld2")
-        initial_conditions = load("data/italy/initial_conditions/$(year)Q$(quarter).jld2")
-        T = 12
-        model = Bit.init_model(parameters, initial_conditions, T)
-        n_sims = 4
-        data_vector = Bit.ensemblerun(model, n_sims)
-        save("data/italy/simulations/$(year)Q$(quarter).jld2", "data_vector", data_vector)
-    end
-end
-
 year_, number_years = 2010, 10
 number_quarters, horizon, number_seeds, number_sectors  = 4 * number_years, 12, 4, 62
+T, n_sims = 12, 4
 
 for year in 2010:2019
     for quarter in 1:4
         date = DateTime(year, quarter*3, daysinmonth(DateTime(year, quarter*3, 1)))
-        sims = load("data/italy/simulations/$(year)Q$(quarter).jld2")["data_vector"]
-        prediction_dict = Bit.get_predictions_from_sims(sims, cal.data, date)
+        parameters = load("data/italy/parameters/$(year)Q$(quarter).jld2")
+        initial_conditions = load("data/italy/initial_conditions/$(year)Q$(quarter).jld2")
+        model = Bit.init_model(parameters, initial_conditions, T)
+        data_vector = Bit.ensemblerun(model, n_sims)
+        prediction_dict = Bit.get_predictions_from_sims(data_vector, cal.data, date)
         save("data/italy/abm_predictions/$(year)Q$(quarter).jld2", "model_dict", prediction_dict)
     end
 end
