1517d0b0f0ac9f021ebff8fe90de3431caab4f9e
tortar <adrianomeligrana1999@gmail.com>
2025-06-15 15:44:41 +0200

    better comments

--- src/utils/varx.jl
+++ src/utils/varx.jl
@@ -1,34 +1,35 @@
 
+"""
+    forecast_k_steps_VAR(data, n_forecasts; intercept = false, lags = 1)
+
+Forecasts the next `n_forecasts` steps of a Vector Autoregression (VAR) model.
+
+# Arguments
+- `data::Matrix{Float64}`: The input time series data, where rows represent time steps and columns represent variables.
+- `n_forecasts::Int`: The number of steps to forecast.
+- `intercept::Bool`: Whether to include an intercept in the VAR model. Default is `false`.
+- `lags::Int`: The number of lags to include in the VAR model. Default is `1`.
+
+# Returns
+- `forecasted_values::Matrix{Float64}`: A matrix containing the forecasted values for the next `k` steps.
+
+# Example
+
+```julia
+N = 500  # Number of time steps
+p = 2  # Number of lags
+alpha = [0.9 -0.2; 0.5 -0.1]  # Coefficient matrix (2 variables, 1 lag)
+alpha = [0.9 -0.2 0.1 0.05; # Coefficient matrix   (2 variables, 2 lags)
+        0.5 -0.1 0.2 -0.3]
+beta = [0.1, -0.2]  # Intercept vector
+sigma = [0.1 0.05; 0.05 0.1]  # Covariance matrix for the noise
+timeseries = generate_var_timeseries(N, p, alpha, beta, sigma)
+alpha_hat, beta_hat, epsilon_hat = BIT.estimate_VAR(timeseries, intercept = false, lags = p)
+# Run the forecast function
+forecast = BIT.forecast_k_steps_VAR(timeseries, 10, intercept = true, lags = p)
+```
+"""
 function forecast_k_steps_VAR(data, n_forecasts; intercept = false, lags = 1, stochastic = false)
-    """
-    
-    forecast_k_steps_VAR(data, k; intercept = false, lags = 1)
-
-    Forecasts the next `k` steps of a Vector Autoregression (VAR) model.
-
-    # Arguments
-    - `data::Matrix{Float64}`: The input time series data, where rows represent time steps and columns represent variables.
-    - `k::Int`: The number of steps to forecast.
-    - `intercept::Bool`: Whether to include an intercept in the VAR model. Default is `false`.
-    - `lags::Int`: The number of lags to include in the VAR model. Default is `1`.
-
-    # Returns
-    - `forecasted_values::Matrix{Float64}`: A matrix containing the forecasted values for the next `k` steps.
-
-    # Example
-    ```julia
-    N = 500  # Number of time steps
-    p = 2  # Number of lags
-    alpha = [0.9 -0.2; 0.5 -0.1]  # Coefficient matrix (2 variables, 1 lag)
-    alpha = [0.9 -0.2 0.1 0.05; # Coefficient matrix   (2 variables, 2 lags)
-            0.5 -0.1 0.2 -0.3]
-    beta = [0.1, -0.2]  # Intercept vector
-    sigma = [0.1 0.05; 0.05 0.1]  # Covariance matrix for the noise
-    timeseries = generate_var_timeseries(N, p, alpha, beta, sigma)
-    alpha_hat, beta_hat, epsilon_hat = BIT.estimate_VAR(timeseries, intercept = false, lags = p)
-    # Run the forecast function
-    forecast = BIT.forecast_k_steps_VAR(timeseries, 10, intercept = true, lags = p)
-    """
 
     forecasted_values = Matrix{Float64}(undef, 0, size(data, 2))
     
@@ -36,18 +37,11 @@ function forecast_k_steps_VAR(data, n_forecasts; intercept = false, lags = 1, st
 
     alpha = dropdims(alpha, dims = tuple(findall(size(alpha) .== 1)...))
 
-    if lags > 1
-        alpha = reshape(alpha,  size(data,2), lags *size(data,2))
-    end
+    lags > 1 && (alpha = reshape(alpha,  size(data,2), lags *size(data,2)))
 
-    if length(alpha) == 1
-        alpha = only(alpha)
-    end
+    length(alpha) == 1 && (alpha = only(alpha))
 
-    if !stochastic
-        epsilon .=0
-    end
-    
+    !stochastic && (epsilon .= 0)
     
     for i in 1:n_forecasts
 
@@ -78,39 +72,34 @@ function forecast_k_steps_VAR(data, n_forecasts; intercept = false, lags = 1, st
     return forecasted_values
 end
 
-function forecast_k_steps_VARX(data, exogenous, n_forecasts; intercept = false, lags = 1, stochastic = false)
-    """
+"""
     
-    forecast_k_steps_VARX(data, k; intercept = false, lags = 1)
+    forecast_k_steps_VARX(data, n_forecasts; intercept = false, lags = 1)
 
-    Forecasts the next `k` steps of a Vector Autoregression with exogenous predictors (VARX) model.
+Forecasts the next `n_forecasts` steps of a Vector Autoregression with exogenous predictors (VARX) model.
 
-    # Arguments
-    - `data::Matrix{Float64}`: The input time series data, where rows represent time steps and columns represent variables.
-    - `exogenous::Matrix{Float64}`: The exogenous time series data, where rows represent time steps and columns represent variables.
-    - `k::Int`: The number of steps to forecast.
-    - `intercept::Bool`: Whether to include an intercept in the VAR model. Default is `false`.
-    - `lags::Int`: The number of lags to include in the VAR model. Default is `1`.
+# Arguments
+- `data::Matrix{Float64}`: The input time series data, where rows represent time steps and columns represent variables.
+- `exogenous::Matrix{Float64}`: The exogenous time series data, where rows represent time steps and columns represent variables.
+- `n_forecasts::Int`: The number of steps to forecast.
+- `intercept::Bool`: Whether to include an intercept in the VAR model. Default is `false`.
+- `lags::Int`: The number of lags to include in the VAR model. Default is `1`.
 
-    # Returns
-    - `forecasted_values::Matrix{Float64}`: A matrix containing the forecasted values for the next `k` steps.
-    """
+# Returns
+- `forecasted_values::Matrix{Float64}`: A matrix containing the forecasted values for the next `n_forecasts` steps.
+"""
+function forecast_k_steps_VARX(data, exogenous, n_forecasts; intercept = false, lags = 1, stochastic = false)
 
-    alpha, beta, gamma, epsilon, sigma = estimate_VARX(data, exogenous[2:size(data,1)+1,:]; intercept = intercept , lags = lags)
+    alpha, beta, gamma, epsilon, sigma = estimate_VARX(data, exogenous[2:size(data,1)+1,:];
+        intercept = intercept , lags = lags)
 
     alpha = dropdims(alpha, dims = tuple(findall(size(alpha) .== 1)...))
 
-    if lags > 1
-        alpha = reshape(alpha,  size(data,2), lags *size(data,2))
-    end
+    lags > 1 && (alpha = reshape(alpha,  size(data,2), lags *size(data,2)))
 
-    if length(alpha) == 1
-        alpha = only(alpha)
-    end
+    length(alpha) == 1 && (alpha = only(alpha))
 
-    if !stochastic
-        epsilon .=0
-    end
+    !stochastic && (epsilon .= 0)
 
     forecasted_values = Matrix{Float64}(undef, 0, size(data, 2))
     
@@ -168,20 +157,11 @@ end
 
 function estimate_VARX(ydata::Union{Matrix{Float64}, Vector{Float64}}, xdata::Union{Matrix{Float64}, Vector{Float64}}; intercept = false, lags = 1)    
 
-    if typeof(ydata) == Vector{Float64}
-        ydata = ydata[:, :]
-    end
-    if typeof(xdata) == Vector{Float64}
-        xdata = xdata[:, :]
-    end
-
-    if intercept
-        xdata = hcat(ones(size(xdata, 1), 1), xdata)
-    end
+    typeof(ydata) == Vector{Float64} && (ydata = ydata[:, :])
+    typeof(xdata) == Vector{Float64} && (xdata = xdata[:, :])
+    intercept && (xdata = hcat(ones(size(xdata, 1), 1), xdata))
         
     var = rfvar3(ydata, lags, xdata)
-    
-    
     alpha = var.By
     beta = var.Bx[:,1]
     gamma = var.Bx[:,2:end]
