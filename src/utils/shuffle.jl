3dae78282991609de9364e9a90fffe7e47f13548
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-07-24 04:15:24 +0200

    Create shuffle.jl

---
+++ src/utils/shuffle.jl
@@ -0,0 +1,10 @@
+
+function faster_shuffle!(vec::AbstractArray)
+    rng = Random.default_rng()
+    for i in 2:length(vec)
+        endi = (i-1) % UInt
+        j = @inline rand(rng, Random.Sampler(rng, UInt(0):endi, Val(1))) % Int + 1
+        vec[i], vec[j] = vec[j], vec[i]
+    end
+    vec
+end
