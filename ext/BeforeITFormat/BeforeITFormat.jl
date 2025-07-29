eee6cc134e3ea706d14bdc10a67a75d798fa0005
tortar <adrianomeligrana1999@gmail.com>
2025-07-30 01:20:08 +0200

    i

--- ext/BeforeITFormat/BeforeITFormat.jl
+++ ext/BeforeITFormat/BeforeITFormat.jl
@@ -4,14 +4,23 @@ import BeforeIT as Bit
 
 using Runic
 
-function Bit.format_package()
+function Bit.format_package(; check=false)
     root_dir = joinpath(@__DIR__, "..", "..")
     for (root, dirs, files) in walkdir(root_dir)
         for file in files
-            endswith(file, ".jl") && Runic.format_file(joinpath(root, file); inplace = true)
+            if endswith(file, ".jl")
+                if check
+                    if Runic.main(["--check", "$(joinpath(root, file))"]) == 1
+                        return false
+                    end
+                else
+                    Runic.format_file(joinpath(root, file); inplace = true)
+                    return
+                end
+            end
         end
     end
-    return
+    return true
 end
 
 end
