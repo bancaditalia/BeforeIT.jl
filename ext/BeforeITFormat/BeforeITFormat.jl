e77f91b739944e0a828d90a1704762904ecc2fe5
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-07-30 00:48:37 +0200

    Update ext/BeforeITFormat/BeforeITFormat.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/BeforeITFormat/BeforeITFormat.jl
+++ ext/BeforeITFormat/BeforeITFormat.jl
@@ -8,7 +8,7 @@ using Runic
 function Bit.format_package()
     for (root, dirs, files) in walkdir(".")
         for file in files
-            endswith(file, ".jl") && Runic.format_file(joinpath(root, file); inplace=true)
+            endswith(file, ".jl") && Runic.format_file(joinpath(root, file); inplace = true)
         end
     end
 end
