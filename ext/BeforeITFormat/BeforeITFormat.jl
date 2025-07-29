cdc9a07359ab3308131db213554d1e1aef6f45aa
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-07-30 00:39:19 +0200

    Create BeforeITFormat.jl

---
+++ ext/BeforeITFormat/BeforeITFormat.jl
@@ -0,0 +1,12 @@
+
+module BeforeITFormat
+
+using Runic
+
+for (root, dirs, files) in walkdir(".")
+    for file in files
+        endswith(file, ".jl") && Runic.format_file(joinpath(root, file); inplace=true)
+    end
+end
+
+end
