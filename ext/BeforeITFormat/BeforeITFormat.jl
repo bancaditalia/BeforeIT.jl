33b4000d039e329ed082d647eb81b97f2a7ab8bd
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-07-30 00:51:24 +0200

    Update BeforeITFormat.jl

--- ext/BeforeITFormat/BeforeITFormat.jl
+++ ext/BeforeITFormat/BeforeITFormat.jl
@@ -6,7 +6,8 @@ import BeforeIT as Bit
 using Runic
 
 function Bit.format_package()
-    for (root, dirs, files) in walkdir(".")
+    root_dir = joinpath(@__DIR__, "..", "..")
+    for (root, dirs, files) in walkdir(root_dir)
         for file in files
             endswith(file, ".jl") && Runic.format_file(joinpath(root, file); inplace = true)
         end
