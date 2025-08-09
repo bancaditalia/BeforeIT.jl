a215bc372d0f94baa772e03d07d9be5d87374f38
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-08-09 03:07:36 +0200

    Update monte_carlo_evaluations.jl

--- test/monte_carlo_evaluations.jl
+++ test/monte_carlo_evaluations.jl
@@ -11,7 +11,7 @@ model = Bit.Model(parameters, initial_conditions)
 
 T = 20
 n_sims = 3
-model_vector = Bit.ensemblerun!((deepcopy(model) for _ in 1:n_sims), t)
+model_vector = Bit.ensemblerun!((deepcopy(model) for _ in 1:n_sims), T)
 data_vector = Bit.DataVector(model_vector)
 
 @test length(data_vector) == n_sims
