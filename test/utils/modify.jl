300cc9326f7a74ca783a00dc573a333e3571e404
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-09-05 02:07:49 +0200

    Update modify.jl

--- test/utils/modify.jl
+++ test/utils/modify.jl
@@ -8,13 +8,8 @@
     agentf = Bit.getfields(agent)
     delete!(model.w_act, id)
     @test !(id in Bit.allids(model.w_act))
-    push!(
-        model.w_act,
-        (
-            Y_h = 1.0, D_h = 1.0, K_h = 1.0, w_h = 1.0, O_h = 1.0,
-            C_d_h = 1.0, I_d_h = 1.0, C_h = 1.0, I_h = 2.0,
-        )
-    )
+    push!(model.w_act, (Y_h = 1.0, D_h = 1.0, K_h = 1.0, w_h = 1.0, O_h = 1.0,
+                        C_d_h = 1.0, I_d_h = 1.0, C_h = 1.0, I_h = 2.0))
     @test model.w_act[length(model.w_act.Y_h) + 1].Y_h == 1.0
     @test model.w_act[length(model.w_act.Y_h) + 1].I_h == 2.0
     push!(model.w_act, agentf)
