937077dc555951e4a825b5fdc55a47139dd58e32
tortar <adrianomeligrana1999@gmail.com>
2025-09-05 16:29:35 +0200

    format

--- examples/single_agents.jl
+++ examples/single_agents.jl
@@ -1,4 +1,3 @@
-
 # # Working with Single Agents
 
 # To support complex extensions of the base model,
@@ -16,7 +15,7 @@ parameters = Bit.AUSTRIA2010Q1.parameters
 initial_conditions = Bit.AUSTRIA2010Q1.initial_conditions
 model = Bit.Model(parameters, initial_conditions);
 
-# Three containers in this model have multiple instances of agents: 
+# Three containers in this model have multiple instances of agents:
 # `model.firms`, `model.w_act`, `model.w_inact`. Each of them
 # to be compatible with this approach contains IDs which correspond
 # to single instances. IDs are `UInt` and are set internally.
@@ -24,7 +23,8 @@ model = Bit.Model(parameters, initial_conditions);
 id = UInt(1)
 agent = model.w_act[id]
 
-# !!! note One invariant of IDs one could rely on is that at initialization
+# !!! note
+# One invariant of IDs one could rely on is that at initialization
 # IDs are mapped one-to-one to indices of the arrays. Though, if any
 # deletion happens this won't be true anymore.
 
@@ -42,7 +42,8 @@ agent.Y_h = 1.0
 
 agentfields = Bit.getfields(agent)
 
-# !!! note Importantly, fields can be accessed as long as the agent is still inside the
+# !!! note
+# Importantly, fields can be accessed as long as the agent is still inside the
 # model, and not after that. So, if you need those fields for something else
 # after removing an agent, retrieve the fields before removing it.
 
