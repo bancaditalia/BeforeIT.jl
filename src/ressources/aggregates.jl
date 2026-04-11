bb0e93724bc7f20bdd9f97c9e795b4d23f2b7dc7
theplatters <fscharnreitner@gmail.com>
2026-04-11 10:39:51 +0200

    aggregate types

---
+++ src/ressources/aggregates.jl
@@ -0,0 +1,43 @@
+struct TimeIndex
+    step:Int64
+end
+
+struct GDP
+  value::Float64
+end
+
+struct Inflation
+  value::Float64
+end
+
+struct GlobalPriceIndex
+  value::Float64
+end
+
+struct PrincipalPriceIndex
+  value::Float64
+end
+
+struct ConsumerPriceIndex
+  value::Float64
+end
+
+struct CapitalPriceIndex
+  value::Float64
+end
+
+struct ExpectedGDP
+  value::Float64
+end
+
+struct ExpectedGrowth
+  rate::Float64
+end
+
+struct ExpectedInflation
+  inflation::Float64
+end
+
+
+
+
