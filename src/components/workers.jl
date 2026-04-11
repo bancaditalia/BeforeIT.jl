1902a18e894f805d9bdc7a07b91116d437c1e700
theplatters <fscharnreitner@gmail.com>
2026-04-11 21:28:32 +0200

    worker creation

--- src/components/workers.jl
+++ src/components/workers.jl
@@ -2,4 +2,7 @@
     wage::Float64
 end
 @component struct Inactive <: AbstractComponent end
-@component struct Unemployed <: AbstractComponent end
+
+@component struct Unemployed <: AbstractComponent
+    unemployment_benefits::Float64
+end
---
+++ src/model_init/workers.jl
@@ -0,0 +1,39 @@
+function setup_workers!(world::Ark.World, properties::Properties)
+    (; active, inactive, total) = properties.population
+    unemployment_benefit_rate = properties.social_insurance.unemployment_benefit
+    (; subsidies_other, subsidies_inactive) = properties.initial_conditions.government
+    (; debt, capital, unemployment_benefit) = properties.initial_conditions.households
+
+    total_firms = properties.dimensions.total_firms
+
+    employable = active - total_firms - 1
+
+    Ark.new_entities!(
+        world, employable, (
+            Components.NetDisposableIncome(0.0),
+            Components.HouseholdDesposit(0.0),
+            Components.CapitalStock(0.0),
+            Components.Unemployed(unemployment_benefit / unemployment_benefit_rate),
+            Components.ConsumptionBudget(0.0),
+            Components.InvestmentBudget(0.0),
+            Components.RealisedConsumption(0.0),
+            Components.RealisedInvestment(0.0),
+        )
+    )
+
+    disposable_income = subsidies_other + subsidies_inactive
+    Ark.new_entities!(
+        world, inactive, (
+            Components.NetDisposableIncome(disposable_income),
+            Components.HouseholdDesposit(debt * disposable_income),
+            Components.CapitalStock(capital * disposable_income),
+            Components.Inactive(),
+            Components.ConsumptionBudget(0.0),
+            Components.InvestmentBudget(0.0),
+            Components.RealisedConsumption(0.0),
+            Components.RealisedInvestment(0.0),
+        )
+    )
+
+    return nothing
+end
