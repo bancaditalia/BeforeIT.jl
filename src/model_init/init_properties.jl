5c27b05812c7a0cfcd3a1cae113e821c07d3cb68
theplatters <fscharnreitner@gmail.com>
2026-04-11 10:40:10 +0200

    rewrote init properties to be more granularily typed

--- src/model_init/init_properties.jl
+++ src/model_init/init_properties.jl
@@ -1,101 +1,166 @@
-abstract type AbstractProperties <: AbstractObject end
-Bit.@object mutable struct Properties(Object) <: AbstractProperties
-    G::Bit.typeInt
-    T_prime::Bit.typeInt
-    H_act::Bit.typeInt
-    H_inact::Bit.typeInt
-    J::Bit.typeInt
-    L::Bit.typeInt
-    I_s::Vector{Bit.typeInt}
-    I::Bit.typeInt
-    H::Bit.typeInt
-    tau_INC::Bit.typeFloat
-    tau_FIRM::Bit.typeFloat
-    tau_VAT::Bit.typeFloat
-    tau_SIF::Bit.typeFloat
-    tau_SIW::Bit.typeFloat
-    tau_EXPORT::Bit.typeFloat
-    tau_CF::Bit.typeFloat
-    tau_G::Bit.typeFloat
-    theta_UB::Bit.typeFloat
-    psi::Bit.typeFloat
-    psi_H::Bit.typeFloat
-    mu::Bit.typeFloat
-    theta_DIV::Bit.typeFloat
-    theta::Bit.typeFloat
-    zeta::Bit.typeFloat
-    zeta_LTV::Bit.typeFloat
-    zeta_b::Bit.typeFloat
-    b_CF_g::Vector{Bit.typeFloat}
-    b_CFH_g::Vector{Bit.typeFloat}
-    b_HH_g::Vector{Bit.typeFloat}
-    c_G_g::Vector{Bit.typeFloat}
-    c_E_g::Vector{Bit.typeFloat}
-    c_I_g::Vector{Bit.typeFloat}
-    a_sg::Matrix{Bit.typeFloat}
-    C::Matrix{Bit.typeFloat}
-    D_H::Bit.typeFloat
-    K_H::Bit.typeFloat
-    sb_other::Bit.typeFloat
-    E_k::Bit.typeFloat
-    r_bar::Bit.typeFloat
+struct Population
+    active::Int64      # H_act: economically active persons
+    inactive::Int64    # H_inact: economically inactive persons
+    total::Int64       # H = active + inactive
+end
+
+struct Dimensions
+    G::Int64           # Number of products/sectors
+    T_prime::Int64     # Time interval for expectations
+    J::Int64           # Number of government entities
+    L::Int64           # Number of foreign consumers
+    I_s::Vector{Int64} # Firms per industry
+    I::Int64           # Total number of firms
+end
+
+# === Tax Rates ===
+struct TaxRates
+    income::Float64           # tau_INC
+    corporate::Float64        # tau_FIRM
+    value_added::Float64      # tau_VAT
+    export::Float64           # tau_EXPORT
+    capital_formation::Float64 # tau_CF
+    government_consumption::Float64 # tau_G
+end
+
+# === Social Insurance ===
+struct SocialInsurance
+    employers_contribution::Float64  # tau_SIF
+    employees_contribution::Float64  # tau_SIW
+    unemployment_benefit::Float64    # theta_UB
+end
+
+# === Household Behavior ===
+struct HouseholdParams
+    consumption_fraction::Float64    # psi
+    housing_investment_fraction::Float64 # psi_H
+end
+
+# === Banking Parameters ===
+struct BankingParams
+    dividend_payout_ratio::Float64   # theta_DIV
+    debt_installment_rate::Float64   # theta
+    capital_requirement::Float64     # zeta
+    loan_to_value_ratio::Float64     # zeta_LTV
+    new_firm_loan_ratio::Float64     # zeta_b
+    risk_premium::Float64            # mu
+end
+
+# === Product/Industry Coefficients ===
+struct ProductCoefficients
+    capital_formation::Vector{Float64}      # b_CF_g
+    household_investment::Vector{Float64}   # b_CFH_g
+    household_consumption::Vector{Float64}  # b_HH_g
+    government_consumption::Vector{Float64} # c_G_g
+    exports::Vector{Float64}                # c_E_g
+    imports::Vector{Float64}                # c_I_g
+    technology::Matrix{Float64}             # a_sg
+    consumption_matrix::Matrix{Float64}     # C
+end
+
+# === Initial Conditions ===
+struct InitialConditions
+    household_debt::Float64        # D_H
+    household_capital::Float64     # K_H
+    other_subsidies::Float64       # sb_other
+    equity_ratio::Float64          # E_k
+    policy_rate::Float64           # r_bar
+end
+
+
+
+mutable struct Properties(Object) <: AbstractProperties
+    dimensions::Dimensions
+    population::Population
+    tax_rates::TaxRates
+    social_insurance::SocialInsurance
+    household_params::HouseholdParams
+    banking_params::BankingParams
+    product_coeffs::ProductCoefficients
+    initial_conditions::InitialConditions
 end
 
 function Properties(parameters::Dict{String, Any}, initial_conditions)
 
-    G = typeInt(parameters["G"])
-    T_prime = typeInt(parameters["T_prime"])       # Time interval used to estimate parameters for expectations
+    # Dimensions
+    G = Int64(parameters["G"])
+    T_prime = Int64(parameters["T_prime"])
+    J = Int64(parameters["J"])
+    L = Int64(parameters["L"])
+    I_s = Vector{Int64}(vec(parameters["I_s"]))
+    I = Int64(sum(parameters["I_s"]))
+    
+    dimensions = Dimensions(G, T_prime, J, L, I_s, I)
 
-    H_act = typeInt(parameters["H_act"])    # Number of economically active persons
-    H_inact = typeInt(parameters["H_inact"])  # Number of economically inactive persons
-    # H = I + H_W + H_inact + 1
+    # Population
+    H_act = Int64(parameters["H_act"])
+    H_inact = Int64(parameters["H_inact"])
+    H = H_act + H_inact
+    
+    population = Population(H_act, H_inact, H)
 
-    J = typeInt(parameters["J"])       # Number of government entities
-    L = typeInt(parameters["L"])        # Number of foreign consumers
-    I_s = Vector{typeInt}(vec(parameters["I_s"]))           # Number of firms/investors in the s-th industry
-    I = typeInt(sum(parameters["I_s"]))        # Number of firms
-    H = H_act + H_inact # Total number of households
+    # Tax Rates
+    tax_rates = TaxRates(
+        Float64(parameters["tau_INC"]),
+        Float64(parameters["tau_FIRM"]),
+        Float64(parameters["tau_VAT"]),
+        Float64(parameters["tau_EXPORT"]),
+        Float64(parameters["tau_CF"]),
+        Float64(parameters["tau_G"])
+    )
 
-    tau_INC = typeFloat(parameters["tau_INC"])    # Income tax rate
-    tau_FIRM = typeFloat(parameters["tau_FIRM"])   # Corporate tax rate
-    tau_VAT = typeFloat(parameters["tau_VAT"])    # Value-added tax rate
-    tau_SIF = typeFloat(parameters["tau_SIF"])    # Social insurance rate (employers’ contributions)
-    tau_SIW = typeFloat(parameters["tau_SIW"])    # Social insurance rate (employees’ contributions)
-    tau_EXPORT = typeFloat(parameters["tau_EXPORT"]) # Export tax rate
-    tau_CF = typeFloat(parameters["tau_CF"])     # Tax rate on capital formation
-    tau_G = typeFloat(parameters["tau_G"])      # Tax rate on government consumption
-    theta_UB = typeFloat(parameters["theta_UB"])   # Unemployment benefit replacement rate
-    psi = typeFloat(parameters["psi"])        # Fraction of income devoted to consumption
-    psi_H = typeFloat(parameters["psi_H"])      # Fraction of income devoted to investment in housing
-    mu = typeFloat(parameters["mu"])         # Risk premium on policy rate
+    # Social Insurance
+    social_insurance = SocialInsurance(
+        Float64(parameters["tau_SIF"]),
+        Float64(parameters["tau_SIW"]),
+        Float64(parameters["theta_UB"])
+    )
 
-    # banking related parameters
-    theta_DIV = typeFloat(parameters["theta_DIV"])  # Dividend payout ratio
-    theta = typeFloat(parameters["theta"])      # Rate of installment on debt
-    zeta = typeFloat(parameters["zeta"])       # Banks’ capital requirement coefficient
-    zeta_LTV = typeFloat(parameters["zeta_LTV"])   # Loan-to-value (LTV) ratio
-    zeta_b = typeFloat(parameters["zeta_b"])     # Loan-to-capital ratio for new firms after bankruptcy
+    # Household Parameters
+    household_params = HouseholdParams(
+        Float64(parameters["psi"]),
+        Float64(parameters["psi_H"])
+    )
 
-    # products related parameters
-    b_CF_g = Vector{typeFloat}(vec(parameters["b_CF_g"]))   # Capital formation coefficient g-th product (firm investment)
-    b_CFH_g = Vector{typeFloat}(vec(parameters["b_CFH_g"])) # Household investment coefficient of the g-th product
-    b_HH_g = Vector{typeFloat}(vec(parameters["b_HH_g"]))   # Consumption coefficient g-th product of households
-    c_G_g = Vector{typeFloat}(vec(parameters["c_G_g"]))     # Consumption of the g-th product of the government in mln. Euro
-    c_E_g = Vector{typeFloat}(vec(parameters["c_E_g"]))     # Exports of the g-th product in mln. Euro
-    c_I_g = Vector{typeFloat}(vec(parameters["c_I_g"]))     # Imports of the gth product in mln. Euro
-    a_sg = Matrix{typeFloat}(parameters["a_sg"])            # Technology coefficient of the gth product in the sth industry
+    # Banking Parameters
+    banking_params = BankingParams(
+        Float64(parameters["theta_DIV"]),
+        Float64(parameters["theta"]),
+        Float64(parameters["zeta"]),
+        Float64(parameters["zeta_LTV"]),
+        Float64(parameters["zeta_b"]),
+        Float64(parameters["mu"])
+    )
 
-    C = Matrix{typeFloat}(parameters["C"])
+    # Product Coefficients
+    product_coeffs = ProductCoefficients(
+        Vector{Float64}(vec(parameters["b_CF_g"])),
+        Vector{Float64}(vec(parameters["b_CFH_g"])),
+        Vector{Float64}(vec(parameters["b_HH_g"])),
+        Vector{Float64}(vec(parameters["c_G_g"])),
+        Vector{Float64}(vec(parameters["c_E_g"])),
+        Vector{Float64}(vec(parameters["c_I_g"])),
+        Matrix{Float64}(parameters["a_sg"]),
+        Matrix{Float64}(parameters["C"])
+    )
 
-    D_H = typeFloat(initial_conditions["D_H"])
-    K_H = typeFloat(initial_conditions["K_H"])
-    sb_other = typeFloat(initial_conditions["sb_other"])
-    E_k = typeFloat(initial_conditions["E_k"])
-    r_bar = typeFloat(initial_conditions["r_bar"])
+    # Initial Conditions
+    init_conds = InitialConditions(
+        Float64(initial_conditions["D_H"]),
+        Float64(initial_conditions["K_H"]),
+        Float64(initial_conditions["sb_other"]),
+        Float64(initial_conditions["E_k"]),
+        Float64(initial_conditions["r_bar"])
+    )
 
     return Properties(
-        G, T_prime, H_act, H_inact, J, L, I_s, I, H, tau_INC, tau_FIRM, tau_VAT, tau_SIF,
-        tau_SIW, tau_EXPORT, tau_CF, tau_G, theta_UB, psi, psi_H, mu, theta_DIV, theta, zeta, zeta_LTV,
-        zeta_b, b_CF_g, b_CFH_g, b_HH_g, c_G_g, c_E_g, c_I_g, a_sg, C, D_H, K_H, sb_other, E_k, r_bar
+        dimensions,
+        population,
+        tax_rates,
+        social_insurance,
+        household_params,
+        banking_params,
+        product_coeffs,
+        init_conds
     )
 end
