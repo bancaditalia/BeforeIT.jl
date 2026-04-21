fbfc4a00650bd9a1f012901c5c1cfdddf1c245ea
Franz Scharnreitner <fscharnreitner@gmail.com>
2026-04-21 10:04:03 +0200

    finished estimation systems

--- src/systems/estimations.jl
+++ src/systems/estimations.jl
@@ -1,4 +1,4 @@
-function set_growth_inflation_expectations!(world)
+function set_growth_inflation_expectations!(world::Ark.World)
     macro_state = Ark.get_resource(world, MacroeconomicState)
     properties = Ark.get_resource(world, Properties)
     interval = properties.dimensions.interval_for_expectation_estimation
@@ -19,20 +19,20 @@ function set_growth_inflation_expectations!(world)
     return nothing
 end
 
-function set_growth_inflation_for_EA!(world)
+function set_growth_inflation_for_EA!(world::Ark.World)
     epsilon_Y_EA = Ark.get_resource(world, Epsilons).Y_EA
     (; inflation_shock_sd, output_outoregression, inflation_response_to_output_gap, inflation_autoregression, output_autoregression_scalar) = Ark.get_resource(world, Properties).external_params
 
     random_inflation_shock = inflation_shock_sd * randn()
 
 
-    for (e, gdp, growth, inflation) in Ark.Query(world, (Components.EuroAreaGPD, Components.EuroAreaGrowth, Components.EuroAreaInflation))
-        for i in eachindex(e)
+    for (e, gdp, growth, inflation) in Ark.Query(world, (Components.EuroAreaGDP, Components.EuroAreaGrowth, Components.EuroAreaInflation))
+        @inbounds for i in eachindex(e)
             expected_growth = exp(output_outoregression * log(gdp[i].value) + output_autoregression_scalar + epsilon_Y_EA)
             growth[i] = Components.EuroAreaGrowth(expected_growth / gdp[i].value)
             gdp[i] = Components.EuroAreaGDP(expected_growth)
             inflation[i] = Components.EuroAreaInflation(
-                exp(inflation_autoregression * log(1 + inflation[i].value) + inflation_response_to_output_gap + random_inflation_shock)
+                exp(inflation_autoregression * log1p(inflation[i].value) + inflation_response_to_output_gap + random_inflation_shock)
             )
         end
     end
@@ -41,7 +41,7 @@ function set_growth_inflation_for_EA!(world)
     return nothing
 end
 
-function set_inflation_price_index!(world)
+function set_inflation_price_index!(world::Ark.World)
     macro_state = Ark.get_resource(world, MacroeconomicState)
     properties = Ark.get_resource(world, Properties)
 
@@ -62,14 +62,41 @@ function set_inflation_price_index!(world)
     return nothing
 end
 
-function set_sector_specific_priceindex!(world)
+function set_sector_specific_priceindex!(world::Ark.World)
     macro_state = Ark.get_resource(world, MacroeconomicState)
     fill!(macro_state.sector_price_index, 0.0)
+    total_quantities = similar(macro_state.sector_price_index)
 
-    for (entities, principal_product, prices, quantities) in Ark.Query(world, (Components.PrincipalProduct, Components.Prices, Components.Quantities))
+    for (entities, principal_product, prices, quantities) in Ark.Query(world, (Components.PrincipalProduct, Components.Price, Components.Quantities))
         @inbounds for i in eachindex(entities)
-
+            macro_state.sector_price_index[principal_product[i].id] += prices[i].value * quantities[i].amount
+            total_quantities[principal_product[i].id] += quantities[i].amount
         end
     end
+
+    for (entities, principal_product, prices, quantities) in Ark.Query(world, (Components.PrincipalProduct, Components.ImportPrice, Components.ImportSales))
+        @inbounds for i in eachindex(entities)
+            macro_state.sector_price_index[principal_product[i].id] += prices[i].value * quantities[i].amount
+            total_quantities[principal_product[i].id] += quantities[i].amount
+        end
+    end
+
+    macro_state.sector_price_index ./= total_quantities
     return nothing
+end
+
+function set_capital_formation_priceindex!(world::Ark.World)
+    macro_state = Ark.get_resource(world, MacroeconomicState)
+    properties = Ark.get_resource(world, Properties)
+    macro_state.capital_goods_price_index = dot(properties.product_coefficients.capital_formation, macro_state.sector_price_index)
+    return nothing
+end
+
+function set_household_price_index!(world::Ark.World)
+    macro_state = Ark.get_resource(world, MacroeconomicState)
+    properties = Ark.get_resource(world, Properties)
+
+    macro_state.household_consumption_price_index = dot(properties.product_coefficients.household_consumption, macro_state.sector_price_index)
+    return nothing
+
 end
