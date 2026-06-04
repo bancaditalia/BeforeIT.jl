c741b841b880c32bfe56e57f2af56d79f69bc632
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-04 04:06:07 +0200

    Update BeforeITMooncake.jl

--- ext/BeforeITMooncake/BeforeITMooncake.jl
+++ ext/BeforeITMooncake/BeforeITMooncake.jl
@@ -3,6 +3,6 @@ module BeforeITMooncake
 import BeforeIT as Bit
 import Mooncake
 
-Mooncake.@zero_derivative Mooncake.MinimalCtx Tuple{typeof(Bit.create_weighted_sampler), Any, Any, Any,}
+Mooncake.@zero_derivative Mooncake.MinimalCtx Tuple{typeof(Bit.create_weighted_sampler), Any, Any, Any}
 
 end
